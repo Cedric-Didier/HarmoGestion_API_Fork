8513a9b3dbfb14741d166539db90aeb6b2c8746c
Thomas <thomasseiwert@hotmail.fr>
2026-04-09 16:48:18 +0200

    FEAT: ajout des attributs dépendants d'autres tables dans le model des cours

--- src/main/java/fr/afpa/cda19/harmogestionapi/models/Cours.java
+++ src/main/java/fr/afpa/cda19/harmogestionapi/models/Cours.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
@@ -19,6 +19,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 /**
  * Model des cours.
@@ -31,7 +32,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "Cours")
+@Table(name = "cours")
 public class Cours {
 
     @Id
@@ -39,30 +40,30 @@ public class Cours {
     @Column(name = "id_cours")
     private Integer idCours;
 
-    @Column(name = "date_cours")
+    @Column(name = "date_cours", nullable = false)
     @NotNull(message = "Le cours doit avoir une date")
     private LocalDateTime dateCours;
 
-    @Column(name = "duree_cours")
+    @Column(name = "duree_cours", nullable = false)
     @Min(value = 30, message = "Le cours doit durer au moins 10 minutes")
     @Max(value = 120, message = "Le cours doit durer au maximum 120 minutes")
     private byte dureeCours;
 
-//    @OneToMany
-//    @JoinColumn(name = "id_membre_enseignant")
-//    @Valid
-//    private Membre enseignant;
-//
-//    @OneToMany
-//    @JoinColumn(name = "id_instrument")
-//    @Valid
-//    private Instrument instrument;
-//
-//    @ManyToMany
-//    @JoinTable(
-//            name = "Participer_Cours",
-//            joinColumns = @JoinColumn(name = "id_membre_apprenant"),
-//            inverseJoinColumns = @JoinColumn(name = "id_cours")
-//    )
-//    private ArrayList<Membre> participants;
+    @ManyToOne
+    @JoinColumn(name = "id_membre_enseignant", nullable = false)
+    @Valid
+    private Membre enseignant;
+
+    @ManyToOne
+    @JoinColumn(name = "id_instrument", nullable = false)
+    @Valid
+    private Instrument instrument;
+
+    @ManyToMany
+    @JoinTable(
+            name = "Participer_Cours",
+            joinColumns = @JoinColumn(name = "id_cours"),
+            inverseJoinColumns = @JoinColumn(name = "id_membre_apprenant")
+    )
+    private ArrayList<Membre> participants;
 }
