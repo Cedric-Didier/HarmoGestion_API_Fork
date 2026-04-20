93e30f4a7fb0dc94083d201b8f14d977f9bc9312
Cédric DIDIER <cedric.did@gmail.com>
2026-04-20 16:43:41 +0200

    test: réactivation des tests désactivés

--- src/test/java/fr/afpa/cda19/harmogestionapi/HarmoGestionApiApplicationTests.java
+++ src/test/java/fr/afpa/cda19/harmogestionapi/HarmoGestionApiApplicationTests.java
@@ -1,15 +1,12 @@
 package fr.afpa.cda19.harmogestionapi;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Disabled
 class HarmoGestionApiApplicationTests {
 
 	@Test
-    @Disabled
 	void contextLoads() {
 	}
 
