7078ce107d94626da2d93accaeb747f5c6b6967c
Cédric DIDIER <cedric.did@gmail.com>
2026-04-23 15:04:09 +0200

    refactor: mise à jour du service en reponse à la modification du repository

--- src/main/java/fr/afpa/cda19/harmogestionapi/services/PratiquerService.java
+++ src/main/java/fr/afpa/cda19/harmogestionapi/services/PratiquerService.java
@@ -1,10 +1,9 @@
 package fr.afpa.cda19.harmogestionapi.services;
 
-import fr.afpa.cda19.harmogestionapi.models.Instrument;
-import fr.afpa.cda19.harmogestionapi.models.Membre;
 import fr.afpa.cda19.harmogestionapi.models.Pratiquer;
 import fr.afpa.cda19.harmogestionapi.models.PratiquerPK;
 import fr.afpa.cda19.harmogestionapi.repositories.PratiquerRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,8 +30,8 @@ public class PratiquerService {
      *
      * @param repository repository des instruments pratiqués
      */
+    @Autowired
     public PratiquerService(
-            @Autowired
             PratiquerRepository repository) {
         this.repository = repository;
     }
@@ -49,24 +48,25 @@ public class PratiquerService {
     /**
      * Récupèration de la liste des instruments pratiqués par un membre.
      *
-     * @param membre Le membre dont on souhaite la liste des instruments
-     *               pratiqués
+     * @param idMembre L'identifiant du membre dont on souhaite la liste
+     *                 des instruments pratiqués
      * @return La liste des instruments pratiqués par le membre
      */
-    public Iterable<Pratiquer> getInstrumentsPratiquesByMembre(
-            final Membre membre) {
-        return repository.findByMembre(membre);
+    public Iterable<Pratiquer> getInstrumentsPratiquesByIdMembre(
+            final Integer idMembre) {
+        return repository.findByIdMembre(idMembre);
     }
 
     /**
      * Récupèration de la liste des membres pratiquant d'un instrument.
      *
-     * @param instrument l'instrument dont on souhaite connaitre les pratiquants
+     * @param idInstrument l'identifiant de l'instrument dont on souhaite
+     *                     connaitre les pratiquants
      * @return La liste des membres pratiquant de cet instrument
      */
-    public Iterable<Pratiquer> getMembresByInstrumentPratique(
-            final Instrument instrument) {
-        return repository.findByInstrument(instrument);
+    public Iterable<Pratiquer> getMembresPratiquantsByIdInstrument(
+            final Integer idInstrument) {
+        return repository.findByIdInstrument(idInstrument);
     }
 
     /**
@@ -87,6 +87,7 @@ public class PratiquerService {
      * @param pratiquer ({@link Pratiquer}) le niveau de pratique à sauvegarder
      * @return le niveau de pratique sauvegardé
      */
+    @Transactional
     public Pratiquer savePratiqueInstrument(final Pratiquer pratiquer) {
         return repository.save(pratiquer);
     }
@@ -97,6 +98,7 @@ public class PratiquerService {
      * @param id ({@link PratiquerPK}) l'ensemble {Instrument, Membre}
      *           constituant l'identifiant de la table
      */
+    @Transactional
     public void deletePratiqueInstrument(final PratiquerPK id) {
         repository.deleteById(id);
     }
@@ -104,20 +106,33 @@ public class PratiquerService {
     /**
      * Supprime la liste des instruments pratiqués d'un membre.
      *
-     * @param membre Le membre dont on souhaite supprimer la liste des
-     *               instruments pratiqués
+     * @param idMembre L'identifiant du membre dont on souhaite supprimer
+     *                 la liste des instruments pratiqués
      */
-    public void deleteAllPratiqueInstrumentByMembre(final Membre membre) {
-        repository.deleteAllByMembre(membre);
+    @Transactional
+    public void deleteAllPratiqueInstrumentByMembre(final Integer idMembre) {
+        repository.deleteAllByIdMembre(idMembre);
     }
 
     /**
      * Supprime la liste des membres pratiquants d'un instrument.
      *
-     * @param instrument L'instrument dont on souhaite supprimer la liste des
-     *                   membres pratiquants
+     * @param idInstrument L'identifiant de l'instrument dont on souhaite
+     *                     supprimer la liste des membres pratiquants
      */
-    public void deleteAllPratiquantByInstrument(final Instrument instrument) {
-        repository.deleteAllByInstrument(instrument);
+    @Transactional
+    public void deleteAllPratiquantByInstrument(final Integer idInstrument) {
+        repository.deleteAllByIdInstrument(idInstrument);
+    }
+
+    /**
+     * Vérifie qu'un membre pratique d'un instrument.
+     *
+     * @param id L'identifiant composé de l'identifiant du membre et
+     *           de l'identifiant de l'instrument
+     * @return True si ce membre pratique cet instrument
+     */
+    public boolean pratiqueExistById(final PratiquerPK id) {
+        return repository.existsById(id);
     }
 }
