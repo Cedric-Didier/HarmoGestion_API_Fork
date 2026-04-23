77434ed93f7e112974cf3f3832bad5b7a476be51
Cédric DIDIER <cedric.did@gmail.com>
2026-04-23 14:43:21 +0200

    refactor: modification de la classe PratiquerPK représentant l'identifiant composite [id_instrument, id_membre]

--- src/main/java/fr/afpa/cda19/harmogestionapi/models/Pratiquer.java
+++ src/main/java/fr/afpa/cda19/harmogestionapi/models/Pratiquer.java
@@ -4,13 +4,19 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Entité représentant la pratique d'un instrument par un membre.
+ *
+ * @author Cédric DIDIER
+ * @version 1.0.0
+ * @since 22/04/2026
+ */
 @Entity
 @Data
 @NoArgsConstructor
@@ -19,16 +25,26 @@ import lombok.NoArgsConstructor;
 @IdClass(PratiquerPK.class)
 public class Pratiquer {
 
+    /**
+     * Identifiant de l'instrument.
+     */
     @Id
-    @ManyToOne
-    @JoinColumn(name = "id_instrument")
-    private Instrument instrument;
+    @Column(name = "id_instrument")
+    @NotNull(message = "l'identifiant de l'instrument est obligatoire.")
+    private Integer idInstrument;
 
+    /**
+     * Identifiant du membre.
+     */
     @Id
-    @ManyToOne
-    @JoinColumn(name = "id_membre")
-    private Membre membre;
+    @Column(name = "id_membre")
+    @NotNull(message = "L'identifiant du membre est obligatoire.")
+    private Integer idMembre;
 
+    /**
+     * Booléen indiquant si la pratique de l'instrument par le membre est en
+     * cours d'apprentissage.
+     */
     @Column(name = "apprentissage_en_cours")
     private Boolean apprentissageEnCours;
 }
--- src/main/java/fr/afpa/cda19/harmogestionapi/models/PratiquerPK.java
+++ src/main/java/fr/afpa/cda19/harmogestionapi/models/PratiquerPK.java
@@ -1,21 +1,34 @@
 package fr.afpa.cda19.harmogestionapi.models;
 
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Classe représentant l'identifiant de la pratique d'un instrument par un
+ * membre.
+ *
+ * @author Cédric DIDIER
+ * @version 1.0.0
+ * @since 22/04/2026
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Embeddable
 public class PratiquerPK {
 
-    @ManyToOne
-    @JoinColumn(name = "id_instrument")
-    private Instrument instrument;
+    /**
+     * Identifiant de l'instrument.
+     */
+    @NotNull(message = "l'identifiant de l'instrument est obligatoire.")
+    private Integer idInstrument;
 
-    @ManyToOne
-    @JoinColumn(name = "id_membre")
-    private Membre membre;
+    /**
+     * Identifiant du membre.
+     */
+    @NotNull(message = "L'identifiant du membre est obligatoire.")
+    private Integer idMembre;
 }
