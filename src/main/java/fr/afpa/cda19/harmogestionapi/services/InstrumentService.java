aa9f2ae1f3a97ead36a5344627228693b63dcc23
Cédric DIDIER <cedric.did@gmail.com>
2026-04-30 16:41:12 +0200

    feat: modification des méthodes getInstruments et getInstrument(id) et ajout de la méthode createInstrument pour commencer la migration vers les DTO

--- src/main/java/fr/afpa/cda19/harmogestionapi/services/InstrumentService.java
+++ src/main/java/fr/afpa/cda19/harmogestionapi/services/InstrumentService.java
@@ -1,11 +1,19 @@
 package fr.afpa.cda19.harmogestionapi.services;
 
+import fr.afpa.cda19.harmogestionapi.dtos.request.InstrumentCreateRequestDTO;
+import fr.afpa.cda19.harmogestionapi.dtos.response.InstrumentResponseDTO;
+import fr.afpa.cda19.harmogestionapi.exceptions.RessourceDupliqueeException;
+import fr.afpa.cda19.harmogestionapi.exceptions.RessourceNonTrouveException;
+import fr.afpa.cda19.harmogestionapi.mappers.InstrumentMapper;
 import fr.afpa.cda19.harmogestionapi.models.Instrument;
 import fr.afpa.cda19.harmogestionapi.repositories.InstrumentRepository;
 import fr.afpa.cda19.harmogestionapi.repositories.PratiquerRepository;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -17,6 +25,7 @@ import java.util.Optional;
  * @since 07/04/2026
  */
 @Service
+@RequiredArgsConstructor
 public class InstrumentService {
 
     /**
@@ -29,28 +38,18 @@ public class InstrumentService {
      */
     private final PratiquerRepository pratiquerRepository;
 
-    /**
-     * Initialisation du service des instruments.
-     *
-     * @param instrumentRepository repository des instruments
-     * @param pratiquerRepository  repository de l'entité Pratiquer
-     */
-    @Autowired
-    public InstrumentService(final InstrumentRepository instrumentRepository,
-                             final PratiquerRepository pratiquerRepository) {
-        this.instrumentRepository = instrumentRepository;
-        this.pratiquerRepository = pratiquerRepository;
-    }
+    private final InstrumentMapper instrumentMapper;
 
     /**
      * Récupèration d'un instrument par son identifiant.
      *
      * @param id l'identifiant de l'instrument recherché
-     * @return ({@link Optional}) l'éventuel instrument correspondant à
-     * l'identifiant
+     * @return ({@link InstrumentResponseDTO}) le dto correspondant à l'instrument recherché
      */
-    public Optional<Instrument> getInstrument(int id) {
-        return instrumentRepository.findById(id);
+    public InstrumentResponseDTO getInstrument(int id) throws RessourceNonTrouveException {
+        Instrument instrument = instrumentRepository.findById(id).orElseThrow(
+                () -> new RessourceNonTrouveException("Impossible de trouver l'instrument"));
+        return instrumentMapper.toDto(instrument);
     }
 
     /**
@@ -58,8 +57,11 @@ public class InstrumentService {
      *
      * @return la liste des instruments
      */
-    public Iterable<Instrument> getInstruments() {
-        return instrumentRepository.findAll();
+    public List<InstrumentResponseDTO> getInstruments() {
+        Iterable<Instrument> instruments = instrumentRepository.findAll();
+        List<InstrumentResponseDTO> listeDto = new ArrayList<>();
+        instruments.forEach(instrument -> listeDto.add(instrumentMapper.toDto(instrument)));
+        return listeDto;
     }
 
     /**
@@ -82,5 +84,16 @@ public class InstrumentService {
     @Transactional
     public Instrument saveInstrument(Instrument instrument) {
         return instrumentRepository.save(instrument);
+    }
+
+    @Transactional
+    public InstrumentResponseDTO createInstrument(InstrumentCreateRequestDTO dto)
+            throws RessourceDupliqueeException {
+        if(instrumentRepository.existsByLibelleInstrument(dto.getNom())){
+            throw new RessourceDupliqueeException("Cet instrument existe déjà");
+        }
+        Instrument instrument = instrumentMapper.toEntity(dto);
+        Instrument savedInstrument = instrumentRepository.save(instrument);
+        return instrumentMapper.toDto(savedInstrument);
     }
 }
