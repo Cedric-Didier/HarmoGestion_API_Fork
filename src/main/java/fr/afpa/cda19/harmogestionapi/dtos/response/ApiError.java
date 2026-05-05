d318b5d2c9dd35c18453d78c6ff96d20977f7844
Cédric DIDIER <cedric.did@gmail.com>
2026-05-05 16:48:43 +0200

    refactor: ajout d'un attribut 'path' au DTO

--- src/main/java/fr/afpa/cda19/harmogestionapi/dtos/response/ApiError.java
+++ src/main/java/fr/afpa/cda19/harmogestionapi/dtos/response/ApiError.java
@@ -3,7 +3,7 @@ package fr.afpa.cda19.harmogestionapi.dtos.response;
 import java.time.LocalDateTime;
 import java.util.Map;
 
-public record ApiError(LocalDateTime timestamp, int status, String message,
+public record ApiError(LocalDateTime timestamp, String path, int status, String message,
                        Map<String, String> details) {
 
 }
